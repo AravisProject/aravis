8b92214368cecc90430ea5a082d36d9853447864
Emmanuel Pacaud <emmanuel@gnome.org>
2010-03-04 20:50:57 +0100

    Add arvtools files.

---
+++ src/arvtools.c
@@ -0,0 +1,8 @@
+#include <arvtools.h>
+
+void *
+arv_unzip (void *buffer, size_t size)
+{
+	return NULL;
+}
+
---
+++ src/arvtools.h
@@ -0,0 +1,12 @@
+#ifndef ARV_TOOLS_H
+#define ARV_TOOLS_H
+
+#include <arv.h>
+
+G_BEGIN_DECLS
+
+void *			arv_unzip (void *buffer, size_t size);
+
+G_END_DECLS
+
+#endif
