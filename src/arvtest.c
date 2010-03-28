fb879c7dacdfc7fa26f91fc9c26d33155f91eaec
Emmanuel Pacaud <emmanuel@gnome.org>
2010-03-29 00:27:24 +0200

    tests: arv-test is now generic.
    Still not tested with the Basler camera yet.

--- src/arvtest.c
+++ src/arvtest.c
@@ -18,29 +18,6 @@ set_cancel (int signal)
 	cancel = TRUE;
 }
 
-typedef struct {
-	guint32 payload_size;
-	guint32 acquisition_control;
-	guint32 acquisition_stop;
-	guint32 acquisition_start;
-} ArvCameraRegisters;
-
-/*static ArvCameraRegisters arv_cameras[] = {*/
-/*        {*/
-/*                .payload_size = 		0xf1f0003c,*/
-/*                .acquisition_control = 		0xf0f00614,*/
-/*                .acquisition_stop =		0x00000000,*/
-/*                .acquisition_start =		0x80000000*/
-/*        },*/
-/*        {*/
-/*                .payload_size = 		0x00012200,*/
-/*                .acquisition_control = 		0x000130f4,*/
-/*                .acquisition_stop =		0x00000000,*/
-/*                .acquisition_start =		0x00000001*/
-/*        }*/
-/*};*/
-
-static char *arv_option_camera_type = "prosilica";
 static int arv_option_debug_level;
 static gboolean arv_option_snaphot = FALSE;
 static gboolean arv_option_auto_buffer = FALSE;
@@ -51,8 +28,6 @@ static int arv_option_vertical_binning = -1;
 
 static const GOptionEntry arv_option_entries[] =
 {
-	{ "type",		't', 0, G_OPTION_ARG_STRING,
-		&arv_option_camera_type,	"Camera type", NULL},
 	{ "snapshot",		's', 0, G_OPTION_ARG_NONE,
 		&arv_option_snaphot,	"Snapshot", NULL},
 	{ "auto",		'a', 0, G_OPTION_ARG_NONE,
@@ -82,15 +57,9 @@ main (int argc, char **argv)
 	ArvDevice *device;
 	ArvStream *stream;
 	ArvBuffer *buffer;
-	ArvCameraType camera_type;
 	GOptionContext *context;
 	GError *error = NULL;
 	char memory_buffer[100000];
-#ifdef PROSILICA
-	char name[ARV_GVBS_USER_DEFINED_NAME_SIZE] = "lapp-vicam02";
-#else
-	char name[ARV_GVBS_USER_DEFINED_NAME_SIZE] = "lapp-vicam01";
-#endif
 	int i;
 #ifdef ARAVIS_WITH_CAIRO
 	gboolean snapshot_done = FALSE;
@@ -112,17 +81,6 @@ main (int argc, char **argv)
 	g_option_context_free (context);
 
 	arv_debug_enable (arv_option_debug_level);
-
-	if (g_strcmp0 (arv_option_camera_type, "basler") == 0)
-		camera_type = ARV_CAMERA_TYPE_BASLER;
-	else if (g_strcmp0 (arv_option_camera_type, "prosilica") == 0)
-		camera_type = ARV_CAMERA_TYPE_PROSILICA;
-	else {
-		g_print ("Unknow camera type: %s\n", arv_option_camera_type);
-		return EXIT_FAILURE;
-	}
-
-	g_print ("Testing %s camera\n", arv_option_camera_type);
 
 	interface = arv_gv_interface_get_instance ();
 
@@ -178,9 +136,6 @@ main (int argc, char **argv)
 		maximum = arv_gc_integer_get_max (ARV_GC_INTEGER (node));
 		g_print ("vertical binning    = %d (max %d)\n", value, maximum);
 
-/*                arv_device_read_register (device, arv_cameras[camera_type].payload_size, &value);*/
-/*                g_print ("payload size = %d (0x%x)\n", value, value);*/
-
 		stream = arv_device_get_stream (device);
 		if (arv_option_auto_buffer)
 			arv_gv_stream_set_option (ARV_GV_STREAM (stream),
@@ -199,16 +154,8 @@ main (int argc, char **argv)
 					ARV_GVBS_USER_DEFINED_NAME,
 					ARV_GVBS_USER_DEFINED_NAME_SIZE, memory_buffer);
 
-		arv_device_write_memory (device,
-					 ARV_GVBS_USER_DEFINED_NAME,
-					 ARV_GVBS_USER_DEFINED_NAME_SIZE, name);
-
 		node = arv_gc_get_node (genicam, "AcquisitionStart");
 		arv_gc_command_execute (ARV_GC_COMMAND (node));
-
-/*                arv_device_write_register (device,*/
-/*                                          arv_cameras[camera_type].acquisition_control,*/
-/*                                          arv_cameras[camera_type].acquisition_start);*/
 
 		signal (SIGINT, set_cancel);
 
@@ -245,10 +192,6 @@ main (int argc, char **argv)
 
 		node = arv_gc_get_node (genicam, "AcquisitionStop");
 		arv_gc_command_execute (ARV_GC_COMMAND (node));
-
-/*                arv_device_write_register (device,*/
-/*                                          arv_cameras[camera_type].acquisition_control,*/
-/*                                          arv_cameras[camera_type].acquisition_stop);*/
 
 		g_object_unref (stream);
 		g_object_unref (device);
