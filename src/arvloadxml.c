4203dbe91a7c4bb4de6d944d8dd90e0cee14fb4b
Emmanuel Pacaud <emmanuel@gnome.org>
2010-03-30 11:40:47 +0200

    tests: add camera name and debug ooptions to arv-load-xml.

--- src/arvloadxml.c
+++ src/arvloadxml.c
@@ -1,28 +1,50 @@
 #include <arv.h>
-#include <arvgvinterface.h>
-#include <arvgvdevice.h>
+#include <stdlib.h>
+
+static char *arv_option_camera_name = NULL;
+static int arv_option_debug_level;
+
+static const GOptionEntry arv_option_entries[] =
+{
+	{ "name",		'n', 0, G_OPTION_ARG_STRING,
+		&arv_option_camera_name,"Camera name", NULL},
+	{ "debug", 		'd', 0, G_OPTION_ARG_INT,
+		&arv_option_debug_level, 	"Debug mode", NULL },
+	{ NULL }
+};
 
 int
 main (int argc, char **argv)
 {
-	ArvInterface *interface;
 	ArvDevice *device;
 	const char *genicam_data;
 	size_t genicam_size;
+	GOptionContext *context;
+	GError *error = NULL;
 
 	g_thread_init (NULL);
 	g_type_init ();
 
-	interface = arv_gv_interface_get_instance ();
+	context = g_option_context_new (NULL);
+	g_option_context_add_main_entries (context, arv_option_entries, NULL);
 
-	device = arv_new_device (NULL);
+	if (!g_option_context_parse (context, &argc, &argv, &error)) {
+		g_option_context_free (context);
+		g_print ("Option parsing failed: %s\n", error->message);
+		g_error_free (error);
+		return EXIT_FAILURE;
+	}
+
+	g_option_context_free (context);
+
+	arv_debug_enable (arv_option_debug_level);
+
+	device = arv_new_device (arv_option_camera_name);
 	if (device != NULL) {
 		genicam_data = arv_device_get_genicam_data (device, &genicam_size);
 		g_file_set_contents ("genicam.xml", genicam_data, genicam_size, NULL);
 	} else
 		g_message ("No device found");
-
-	g_object_unref (interface);
 
 	return 0;
 }
