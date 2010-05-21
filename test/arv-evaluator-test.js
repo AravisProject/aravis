24a277edfd2e9bd65e0dd1c36e3062c412fe1a1c
Emmanuel Pacaud <emmanuel@gnome.org>
2010-05-21 11:26:46 +0200

    introspection: 2 more gjs scripts.

---
+++ test/arv-evaluator-test.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env gjs
+
+/* Aravis - Digital camera library
+ *
+ * Copyright © 2009-2010 Emmanuel Pacaud
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Emmanuel Pacaud <emmanuel@gnome.org>
+ */
+
+/*
+   If you have installed aravis in a non standard location, you may need
+   to make GI_TYPELIB_PATH point to the correct location. For example:
+
+   export GI_TYPELIB_PATH=$GI_TYPELIB_PATH:/opt/bin/lib/girepositry-1.0/
+
+   You may also have to give the path to libaravis.so, using LD_PRELOAD or
+   LD_LIBRARY_PATH.
+ */
+
+const GLib = imports.gi.GLib;
+const Aravis = imports.gi.Aravis;
+
+let evaluator = Aravis.Evaluator.new ("1+2*4.4");
+
+let int_result = evaluator.evaluate_as_int64 ();
+let dbl_result = evaluator.evaluate_as_double ();
+
+log (int_result);
+log (dbl_result);
+
+evaluator.set_expression ("VAR+10");
+evaluator.set_double_variable ("VAR", 1.2);
+
+let dbl_result = evaluator.evaluate_as_double ();
+
+log (dbl_result);
