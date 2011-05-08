2b651f51b59cb96e79a78499b06eb5ff61be6d81
Emmanuel Pacaud <emmanuel@gnome.org>
2011-05-08 20:42:06 +0200

    viewer: make it compile using gtk+-3.0.

--- viewer/arvviewer.c
+++ viewer/arvviewer.c
@@ -338,6 +338,7 @@ arv_viewer_select_camera_cb (GtkComboBox *combo_box, ArvViewer *viewer)
 {
 	GtkTreeIter iter;
 	GtkTreeModel *list_store;
+	GdkWindow *window;
 	GstCaps *caps;
 	GstElement *ffmpegcolorspace;
 	GstElement *ximagesink;
@@ -467,7 +468,8 @@ arv_viewer_select_camera_cb (GtkComboBox *combo_box, ArvViewer *viewer)
 
 	gst_element_set_state (viewer->pipeline, GST_STATE_PLAYING);
 
-	window_xid = GDK_WINDOW_XID (viewer->drawing_area->window);
+	window = gtk_widget_get_window (viewer->drawing_area);
+	window_xid = GDK_WINDOW_XID (window);
 	gst_x_overlay_set_xwindow_id (GST_X_OVERLAY (ximagesink), window_xid);
 
 	g_signal_connect (viewer->stream, "new-buffer", G_CALLBACK (arv_viewer_new_buffer_cb), viewer);
