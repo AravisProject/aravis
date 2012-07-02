1e68bfee11d70432586bdef4fd7330a503ac8081
Jure Varlec <jure.varlec@ad-vega.si>
2012-07-02 10:12:49 +0200

    Export arv_gc_feature_node_get_display_name declaration.

--- src/arvgcfeaturenode.h
+++ src/arvgcfeaturenode.h
@@ -61,6 +61,7 @@ const char *		arv_gc_feature_node_get_name			(ArvGcFeatureNode *gc_feature_node)
 
 const char *		arv_gc_feature_node_get_tooltip			(ArvGcFeatureNode *gc_feature_node, GError **error);
 const char *		arv_gc_feature_node_get_description		(ArvGcFeatureNode *gc_feature_node, GError **error);
+const char *		arv_gc_feature_node_get_display_name		(ArvGcFeatureNode *node, GError **error);
 
 gboolean		arv_gc_feature_node_is_available		(ArvGcFeatureNode *gc_feature_node, GError **error);
 gboolean		arv_gc_feature_node_is_implemented		(ArvGcFeatureNode *gc_feature_node, GError **error);
