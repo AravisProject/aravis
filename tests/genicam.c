70b0495c8656b4f54f855b00d73905e410ea2fb7
Emmanuel Pacaud <emmanuel@gnome.org>
2013-04-29 20:28:04 +0200

    tests: add a test for &amp;&amp; entities.

--- tests/genicam.c
+++ tests/genicam.c
@@ -250,6 +250,12 @@ swiss_knife_test (void)
 	value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
 	g_assert_cmpint (value, ==, 0x10305070);
 
+	node = arv_gc_get_node (genicam, "IntSwissKnifeTestEntity2");
+	g_assert (ARV_IS_GC_SWISS_KNIFE (node));
+
+	value = arv_gc_integer_get_value (ARV_GC_INTEGER (node), NULL);
+	g_assert_cmpint (value, ==, 3);
+
 	g_object_unref (device);
 }
 
