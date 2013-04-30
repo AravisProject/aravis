fdd15cc071bfbe428f3519d9d8c328bf16b12398
Edgar Thier <edgarthier@gmail.com>
2013-04-30 21:59:25 +0200

    camera: handle enumerated framerate of The Imaging Source cameras.

--- src/arvcamera.h
+++ src/arvcamera.h
@@ -91,6 +91,7 @@ gboolean 	arv_camera_is_frame_rate_available 	(ArvCamera *camera);
 
 void		arv_camera_set_frame_rate		(ArvCamera *camera, double frame_rate);
 double 		arv_camera_get_frame_rate 		(ArvCamera *camera);
+void		arv_camera_get_frame_rate_bounds 	(ArvCamera * camera, double *min, double *max);
 void		arv_camera_set_trigger			(ArvCamera *camera, const char *source);
 void 		arv_camera_set_trigger_source		(ArvCamera *camera, const char *source);
 const char *	arv_camera_get_trigger_source		(ArvCamera *camera);
