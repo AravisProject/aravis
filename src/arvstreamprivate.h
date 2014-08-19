4306e951d7105162a5ad6d0594e88784b9ebdf8e
Emmanuel Pacaud <emmanuel@gnome.org>
2014-08-19 23:58:29 +0200

    stream: add missing header

---
+++ src/arvstreamprivate.h
@@ -0,0 +1,35 @@
+/* Aravis - Digital camera library
+ *
+ * Copyright © 2009-2010 Emmanuel Pacaud
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Author: Emmanuel Pacaud <emmanuel@gnome.org>
+ */
+
+#ifndef ARV_STREAM_PRIVATE_H
+#define ARV_STREAM_PRIVATE_H
+
+#include <arvstream.h>
+
+G_BEGIN_DECLS
+
+ArvBuffer *	arv_stream_pop_input_buffer		(ArvStream *stream);
+void		arv_stream_push_output_buffer		(ArvStream *stream, ArvBuffer *buffer);
+
+G_END_DECLS
+
+#endif
