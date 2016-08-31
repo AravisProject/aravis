7c6e0b203d014c0ee2c6dc789de4c5cb1af2132f
Emmanuel Pacaud <emmanuel@gnome.org>
2016-08-31 14:44:44 +0200

    python: remove copyright notice in python sample codes

--- tests/python/arv-evaluator-test.py
+++ tests/python/arv-evaluator-test.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python
 
-# Aravis - Digital camera library
-#
-# Copyright (c) 2011 Emmanuel Pacaud
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General
-# Public License along with this library; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301, USA.
-#
-# Author: Emmanuel Pacaud <emmanuel@gnome.org>
-
 #  If you have installed aravis in a non standard location, you may need
 #   to make GI_TYPELIB_PATH point to the correct location. For example:
 #
