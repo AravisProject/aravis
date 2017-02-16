8b78a1e5436c4fee4ddd550ec42e1ae4f440b175
Emmanuel Pacaud <emmanuel@gnome.org>
2017-02-16 11:27:12 +0100

    build: fix compilation on older systems

--- autogen.sh
+++ autogen.sh
@@ -22,7 +22,7 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
         echo "" >&2
 fi
 
-aclocal --install || exit 1
+aclocal --install -I m4 || exit 1
 intltoolize --force --copy --automake || exit 1
 gtkdocize --copy || exit 1
 autoreconf --verbose --force --install || exit 1
