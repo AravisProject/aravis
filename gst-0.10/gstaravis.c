3c96cf019b776aa9ebc2a544b22dcac077089089
Emmanuel Pacaud <emmanuel@gnome.org>
2017-02-17 11:04:40 +0100

    gst-0.10: cast buffer data to guint8 *

--- gst-0.10/gstaravis.c
+++ gst-0.10/gstaravis.c
@@ -402,7 +402,7 @@ gst_aravis_create (GstPushSrc * push_src, GstBuffer ** buffer)
 		GST_BUFFER_MALLOCDATA (buffer) = data;
 		GST_BUFFER_SIZE (buffer) = size;
 	} else {
-		GST_BUFFER_DATA (*buffer) = buffer_data;
+		GST_BUFFER_DATA (*buffer) = (guint8 *) buffer_data;
 		GST_BUFFER_MALLOCDATA (*buffer) = NULL;
 		GST_BUFFER_SIZE (*buffer) = buffer_size;
 	}
