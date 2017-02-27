4a51fb1597072540af9be95844a8858cdd04527f
Emmanuel Pacaud <emmanuel@gnome.org>
2017-02-27 21:46:21 +0100

    tests: improve code coverage for ArvBuffer

--- tests/fakegv.c
+++ tests/fakegv.c
@@ -71,12 +71,27 @@ acquisition_test (void)
 {
 	ArvCamera *camera;
 	ArvBuffer *buffer;
+	gint x, y, width, height;
 
 	camera = arv_camera_new ("Aravis-GV01");
 	g_assert (ARV_IS_CAMERA (camera));
 
 	buffer = arv_camera_acquisition (camera, 0);
 	g_assert (ARV_IS_BUFFER (buffer));
+
+	arv_buffer_get_image_region (buffer, &x, &y, &width, &height);
+
+	g_assert_cmpint (x, ==, 0);
+	g_assert_cmpint (y, ==, 0);
+	g_assert_cmpint (width, ==, 1024);
+	g_assert_cmpint (height, ==, 1024);
+
+	g_assert_cmpint (arv_buffer_get_image_x (buffer), ==, 0);
+	g_assert_cmpint (arv_buffer_get_image_y (buffer), ==, 0);
+	g_assert_cmpint (arv_buffer_get_image_width (buffer), ==, 1024);
+	g_assert_cmpint (arv_buffer_get_image_height (buffer), ==, 1024);
+
+	g_assert (arv_buffer_get_image_pixel_format (buffer) == ARV_PIXEL_FORMAT_MONO_8);
 
 	g_clear_object (&camera);
 	g_clear_object (&buffer);
