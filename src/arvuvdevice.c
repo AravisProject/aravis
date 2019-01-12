7f7d265fc15581818adeca657e5f4feb2e26372b
wuyuanyi <wuyuanyi135@gmail.com>
2019-01-12 00:29:52 -0500

    memory leak: genicam_xml not released

--- src/arvuvdevice.c
+++ src/arvuvdevice.c
@@ -731,6 +731,7 @@ arv_uv_device_finalize (GObject *object)
 	g_clear_pointer (&uv_device->priv->vendor, g_free);
 	g_clear_pointer (&uv_device->priv->product, g_free);
 	g_clear_pointer (&uv_device->priv->serial_nbr, g_free);
+	g_clear_pointer (&uv_device->priv->genicam_xml, g_free);
 	if (uv_device->priv->usb_device != NULL) {
 		libusb_release_interface (uv_device->priv->usb_device, uv_device->priv->control_interface);
 		libusb_release_interface (uv_device->priv->usb_device, uv_device->priv->data_interface);
