a60cd79996de070e0273ed563dae8c402e9f6f22
Emmanuel Pacaud <emmanuel@gnome.org>
2019-10-22 10:18:07 +0200

    gcregisterdescriptionnode: use gobject macros

--- src/arvgcregisterdescriptionnode.h
+++ src/arvgcregisterdescriptionnode.h
@@ -33,32 +33,8 @@
 G_BEGIN_DECLS
 
 #define ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE             (arv_gc_register_description_node_get_type ())
-#define ARV_GC_REGISTER_DESCRIPTION_NODE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE, ArvGcRegisterDescriptionNode))
-#define ARV_GC_REGISTER_DESCRIPTION_NODE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE, ArvGcRegisterDescriptionNodeClass))
-#define ARV_IS_GC_REGISTER_DESCRIPTION_NODE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE))
-#define ARV_IS_GC_REGISTER_DESCRIPTION_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE))
-#define ARV_GC_REGISTER_DESCRIPTION_NODE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj), ARV_TYPE_GC_REGISTER_DESCRIPTION_NODE, ArvGcRegisterDescriptionNodeClass))
+G_DECLARE_FINAL_TYPE (ArvGcRegisterDescriptionNode, arv_gc_register_description_node, ARV, GC_REGISTER_DESCRIPTION_NODE, ArvGcFeatureNode)
 
-typedef struct _ArvGcRegisterDescriptionNodeClass ArvGcRegisterDescriptionNodeClass;
-
-struct _ArvGcRegisterDescriptionNode {
-	ArvGcFeatureNode	node;
-
-	char *model_name;
-	char *vendor_name;
-	guint major_version;
-	guint minor_version;
-	guint subminor_version;
-	guint schema_major_version;
-	guint schema_minor_version;
-	guint schema_subminor_version;
-};
-
-struct _ArvGcRegisterDescriptionNodeClass {
-	ArvGcFeatureNodeClass parent_class;
-};
-
-GType 		arv_gc_register_description_node_get_type 		(void);
 ArvGcNode * 	arv_gc_register_description_node_new 			(void);
 int		arv_gc_register_description_node_compare_schema_version	(ArvGcRegisterDescriptionNode *node,
 									 guint major,
