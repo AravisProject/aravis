AravisProject/aravis

commit d0de78275acf6d1c549e7aae91ef827dafcd10be

build: add MinGW-w64 support

src/arvgvfakecamera.c
@@ -26,8 +26,8 @@
 #include <arvgvcpprivate.h>
 #include <arvgvspprivate.h>
 #include <arvmisc.h>
-#include <net/if.h>
-#include <ifaddrs.h>
+#include <arvmiscprivate.h>
+#include <arvnetworkprivate.h>
 
 /**
  * SECTION: arvgvfakecamera
@@ -474,86 +474,77 @@ _create_and_bind_input_socket (GSocket **socket_out, const char *socket_name, GI
 static gboolean
 arv_gv_fake_camera_start (ArvGvFakeCamera *gv_fake_camera)
 {
-	struct ifaddrs *ifap = NULL;
-	struct ifaddrs *ifap_iter;
-	int return_value;
+	GList *ifaces;
+	GList *iface_iter;
 	gboolean interface_found = FALSE;
 	gboolean success = TRUE;
 
 	g_return_val_if_fail (ARV_IS_GV_FAKE_CAMERA (gv_fake_camera), FALSE);
 
-	return_value = getifaddrs (&ifap);
-	if (return_value < 0) {
+	ifaces = arv_enumerate_network_interfaces ();
+	if (!ifaces) {
 		arv_warning_device ("[GvFakeCamera::start] No network interface found");
 		return FALSE;
 	}
 
-	for (ifap_iter = ifap ;ifap_iter != NULL && !interface_found; ifap_iter = ifap_iter->ifa_next) {
-		if ((ifap_iter->ifa_flags & IFF_UP) != 0 &&
-		    (ifap_iter->ifa_flags & IFF_POINTOPOINT) == 0 &&
-		    (ifap_iter->ifa_addr->sa_family == AF_INET) &&
-		    g_strcmp0 (ifap_iter->ifa_name, gv_fake_camera->priv->interface_name) == 0) {
-			GSocketAddress *socket_address;
-			GInetAddress *inet_address;
-			GInetAddress *gvcp_inet_address;
-			unsigned int n_socket_fds;
-			unsigned int i;
+	for (iface_iter = ifaces; iface_iter != NULL; iface_iter = iface_iter->next) {
+		GSocketAddress *socket_address;
+		GInetAddress *inet_address;
+		GInetAddress *gvcp_inet_address;
+		unsigned int n_socket_fds;
+		unsigned int i;
+		socket_address = g_socket_address_new_from_native (arv_network_interface_get_addr(iface_iter->data),
+								   sizeof (struct sockaddr));
+		gvcp_inet_address = g_object_ref (g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address)));
+		g_clear_object (&socket_address);
+		arv_fake_camera_set_inet_address (gv_fake_camera->priv->camera, gvcp_inet_address);
 
-			socket_address = g_socket_address_new_from_native (ifap_iter->ifa_addr,
-									   sizeof (struct sockaddr));
-			gvcp_inet_address = g_object_ref (g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address)));
-			g_clear_object (&socket_address);
-			arv_fake_camera_set_inet_address (gv_fake_camera->priv->camera, gvcp_inet_address);
+		success = success && _create_and_bind_input_socket (&gv_fake_camera->priv->gvsp_socket,
+								    "GVSP", gvcp_inet_address, 0, FALSE, FALSE);
+		success = success && _create_and_bind_input_socket
+			(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_GVCP],
+			 "GVCP", gvcp_inet_address, ARV_GVCP_PORT, FALSE, FALSE);
 
-			success = success && _create_and_bind_input_socket (&gv_fake_camera->priv->gvsp_socket,
-									    "GVSP", gvcp_inet_address, 0, FALSE, FALSE);
+		inet_address = g_inet_address_new_from_string ("255.255.255.255");
+		if (!g_inet_address_equal (gvcp_inet_address, inet_address))
 			success = success && _create_and_bind_input_socket
-				(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_GVCP],
-				 "GVCP", gvcp_inet_address, ARV_GVCP_PORT, FALSE, FALSE);
+				(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_GLOBAL_DISCOVERY],
+				 "Global discovery", inet_address, ARV_GVCP_PORT, TRUE, FALSE);
+		g_clear_object (&inet_address);
+		socket_address = g_socket_address_new_from_native (arv_network_interface_get_broadaddr(iface_iter->data),
+								   sizeof (struct sockaddr));
+		inet_address = g_object_ref (g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address)));
+		g_clear_object (&socket_address);
+		if (!g_inet_address_equal (gvcp_inet_address, inet_address))
+			success = success && _create_and_bind_input_socket
+				(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_SUBNET_DISCOVERY],
+				 "Subnet discovery", inet_address, ARV_GVCP_PORT, FALSE, FALSE);
+		g_clear_object (&inet_address);
 
-			inet_address = g_inet_address_new_from_string ("255.255.255.255");
-			if (!g_inet_address_equal (gvcp_inet_address, inet_address))
-				success = success && _create_and_bind_input_socket 
-					(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_GLOBAL_DISCOVERY],
-					 "Global discovery", inet_address, ARV_GVCP_PORT, TRUE, FALSE);
-			g_clear_object (&inet_address);
+		g_clear_object (&gvcp_inet_address);
 
-			socket_address = g_socket_address_new_from_native (ifap_iter->ifa_broadaddr,
-									   sizeof (struct sockaddr));
-                        inet_address = g_object_ref (g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address)));
-			g_clear_object (&socket_address);
-			if (!g_inet_address_equal (gvcp_inet_address, inet_address))
-				success = success && _create_and_bind_input_socket 
-					(&gv_fake_camera->priv->input_sockets[ARV_GV_FAKE_CAMERA_INPUT_SOCKET_SUBNET_DISCOVERY],
-					 "Subnet discovery", inet_address, ARV_GVCP_PORT, FALSE, FALSE);
-			g_clear_object (&inet_address);
+		n_socket_fds = 0;
+		if (success) {
+			for (i = 0; i < ARV_GV_FAKE_CAMERA_N_INPUT_SOCKETS; i++) {
+				GSocket *socket = gv_fake_camera->priv->input_sockets[i];
 
-			g_clear_object (&gvcp_inet_address);
+				if (G_IS_SOCKET (socket)) {
+					gv_fake_camera->priv->socket_fds[n_socket_fds].fd = g_socket_get_fd (socket);
+					gv_fake_camera->priv->socket_fds[n_socket_fds].events = G_IO_IN;
+					gv_fake_camera->priv->socket_fds[n_socket_fds].revents = 0;
 
-			n_socket_fds = 0;
-			if (success) {
-				for (i = 0; i < ARV_GV_FAKE_CAMERA_N_INPUT_SOCKETS; i++) {
-					GSocket *socket = gv_fake_camera->priv->input_sockets[i];
-
-					if (G_IS_SOCKET (socket)) {
-						gv_fake_camera->priv->socket_fds[n_socket_fds].fd = g_socket_get_fd (socket);
-						gv_fake_camera->priv->socket_fds[n_socket_fds].events = G_IO_IN;
-						gv_fake_camera->priv->socket_fds[n_socket_fds].revents = 0;
-
-						n_socket_fds++;
-					}
+					n_socket_fds++;
 				}
-
-				arv_debug_device ("Listening to %d sockets", n_socket_fds);
 			}
 
-			gv_fake_camera->priv->n_socket_fds = n_socket_fds;
-
-			interface_found = TRUE;
+			arv_debug_device ("Listening to %d sockets", n_socket_fds);
 		}
-	}
 
-	freeifaddrs (ifap);
+		gv_fake_camera->priv->n_socket_fds = n_socket_fds;
+
+		interface_found = TRUE;
+	}
+	g_list_free_full (ifaces, (GDestroyNotify) arv_network_interface_free);
 
 	if (!success) {
 		unsigned int i;

meson.build
@@ -50,6 +50,12 @@ if usb_dep.found()
   aravis_dependencies += usb_dep
 endif
 
+if host_machine.system()=='windows'
+	# socket functions need this library under Windows (winsock2)
+	ws2_32 = cc.find_library('ws2_32', required: true)
+	aravis_dependencies += ws2_32
+endif
+
 has_if_packet = cc.has_header (join_paths ('linux','if_packet.h'))
 packet_socket_option = get_option('packet-socket')
 if packet_socket_option.enabled()

src/arvgvdevice.c
@@ -35,43 +35,15 @@
 #include <arvgvstreamprivate.h>
 #include <arvgvcpprivate.h>
 #include <arvgvspprivate.h>
+#include <arvnetworkprivate.h>
 #include <arvzip.h>
 #include <arvstr.h>
 #include <arvmisc.h>
 #include <arvenumtypes.h>
 #include <string.h>
 #include <stdlib.h>
-#ifndef __APPLE__
-#include <linux/ip.h>
-#endif
-#include <netinet/udp.h>
 
 /* Shared data (main thread - heartbeat) */
-
-#ifdef __APPLE__
-struct iphdr
-  {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    unsigned int ihl:4;
-    unsigned int version:4;
-#elif __BYTE_ORDER == __BIG_ENDIAN
-    unsigned int version:4;
-    unsigned int ihl:4;
-#else
-# error  "Please fix <bits/endian.h>"
-#endif
-    u_int8_t tos;
-    u_int16_t tot_len;
-    u_int16_t id;
-    u_int16_t frag_off;
-    u_int8_t ttl;
-    u_int8_t protocol;
-    u_int16_t check;
-    u_int32_t saddr;
-    u_int32_t daddr;
-    /*The options start here. */
-  };
-#endif
 
 enum
 {

src/arvgvinterface.c
@@ -25,24 +25,20 @@
  * @short_description: GigEVision interface
  */
 
-#include <arpa/inet.h>
 #include <arvgvinterfaceprivate.h>
 #include <arvinterfaceprivate.h>
 #include <arvgvdeviceprivate.h>
 #include <arvgvcpprivate.h>
 #include <arvdebugprivate.h>
 #include <arvmisc.h>
+#include <arvmiscprivate.h>
+#include <arvnetworkprivate.h>
 #include <arvstr.h>
 #include <glib/gprintf.h>
 #include <gio/gio.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <net/if.h>
-#include <netdb.h>
-#include <ifaddrs.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netinet/in.h>
 
 /* ArvGvDiscoverSocket implementation */
 
@@ -64,19 +60,6 @@ arv_gv_discover_socket_set_broadcast (ArvGvDiscoverSocket *discover_socket, gboo
 	return result == 0;
 }
 
-static gboolean
-arv_gv_discover_socket_set_buffer_size (ArvGvDiscoverSocket *discover_socket, gint buffer_size)
-{
-	int socket_fd;
-	int result;
-
-	socket_fd = g_socket_get_fd (discover_socket->socket);
-
-	result = setsockopt (socket_fd, SOL_SOCKET, SO_RCVBUF, &buffer_size, sizeof (buffer_size));
-
-	return result == 0;
-}
-
 typedef struct {
 	unsigned int n_sockets;
 	GSList *sockets;
@@ -88,48 +71,42 @@ arv_gv_discover_socket_list_new (void)
 {
 	ArvGvDiscoverSocketList *socket_list;
 	GSList *iter;
-	struct ifaddrs *ifap  = NULL;
-	struct ifaddrs *ifap_iter;
+	GList *ifaces;
+	GList *iface_iter;
 	int i;
 
 	socket_list = g_new0 (ArvGvDiscoverSocketList, 1);
 
-	if (getifaddrs (&ifap) < 0)
+	ifaces = arv_enumerate_network_interfaces ();
+	if (!ifaces)
 		return socket_list;
 
-	for (ifap_iter = ifap; ifap_iter != NULL; ifap_iter = ifap_iter->ifa_next) {
-		if ((ifap_iter->ifa_flags & IFF_UP) != 0 &&
-		    (ifap_iter->ifa_flags & IFF_POINTOPOINT) == 0 &&
-		    (ifap_iter->ifa_addr != NULL) &&
-		    (ifap_iter->ifa_addr->sa_family == AF_INET)) {
-			ArvGvDiscoverSocket *discover_socket = g_new0 (ArvGvDiscoverSocket, 1);
-			GSocketAddress *socket_address;
-			GInetAddress *inet_address;
-			char *inet_address_string;
-			GError *error = NULL;
-			gint buffer_size = ARV_GV_INTERFACE_DISCOVERY_SOCKET_BUFFER_SIZE;
+	for (iface_iter = ifaces; iface_iter != NULL; iface_iter = iface_iter->next) {
+		ArvGvDiscoverSocket *discover_socket = g_new0 (ArvGvDiscoverSocket, 1);
+		GSocketAddress *socket_address;
+		GInetAddress *inet_address;
+		char *inet_address_string;
+		GError *error = NULL;
+		gint buffer_size = ARV_GV_INTERFACE_DISCOVERY_SOCKET_BUFFER_SIZE;
+		socket_address = g_socket_address_new_from_native (arv_network_interface_get_addr(iface_iter->data),
+									sizeof (struct sockaddr));
+		inet_address = g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address));
+		inet_address_string = g_inet_address_to_string (inet_address);
+		arv_debug_interface ("[GvDiscoverSocket::new] Add interface %s", inet_address_string);
+		g_free (inet_address_string);
+		discover_socket->interface_address = g_inet_socket_address_new (inet_address, 0);
+		g_object_unref (socket_address);
 
-			socket_address = g_socket_address_new_from_native (ifap_iter->ifa_addr,
-									   sizeof (struct sockaddr));
-			inet_address = g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (socket_address));
-			inet_address_string = g_inet_address_to_string (inet_address);
-			arv_debug_interface ("[GvDiscoverSocket::new] Add interface %s", inet_address_string);
-			g_free (inet_address_string);
-			discover_socket->interface_address = g_inet_socket_address_new (inet_address, 0);
-			g_object_unref (socket_address);
+		discover_socket->socket = g_socket_new (G_SOCKET_FAMILY_IPV4,
+							G_SOCKET_TYPE_DATAGRAM,
+							G_SOCKET_PROTOCOL_UDP, NULL);
+		arv_socket_set_recv_buffer_size (g_socket_get_fd (discover_socket->socket), buffer_size);
+		g_socket_bind (discover_socket->socket, discover_socket->interface_address, FALSE, &error);
 
-			discover_socket->socket = g_socket_new (G_SOCKET_FAMILY_IPV4,
-								G_SOCKET_TYPE_DATAGRAM,
-								G_SOCKET_PROTOCOL_UDP, NULL);
-			arv_gv_discover_socket_set_buffer_size (discover_socket, buffer_size);
-			g_socket_bind (discover_socket->socket, discover_socket->interface_address, FALSE, &error);
-
-			socket_list->sockets = g_slist_prepend (socket_list->sockets, discover_socket);
-			socket_list->n_sockets++;
-		}
+		socket_list->sockets = g_slist_prepend (socket_list->sockets, discover_socket);
+		socket_list->n_sockets++;
 	}
-
-	freeifaddrs (ifap);
+	g_list_free_full (ifaces, (GDestroyNotify) arv_network_interface_free);
 
 	socket_list->poll_fds = g_new (GPollFD, socket_list->n_sockets);
 	for (i = 0, iter = socket_list->sockets; iter != NULL; i++, iter = iter->next) {
@@ -485,40 +462,34 @@ arv_gv_interface_camera_locate (ArvGvInterface *gv_interface, GInetAddress *devi
 	GSocketAddress *device_socket_address;
 	size_t size;
 	int i, count;
-
-	struct ifaddrs *ifap = NULL;
-	struct ifaddrs *ifap_iter;
+	GList *ifaces;
+	GList *iface_iter;
 	struct sockaddr_in device_sockaddr;
 
 	device_socket_address = g_inet_socket_address_new(device_address, ARV_GVCP_PORT);
 
-	if (getifaddrs(&ifap) >= 0) {
+	ifaces = arv_enumerate_network_interfaces ();
+	if (ifaces) {
 		g_socket_address_to_native(device_socket_address, &device_sockaddr, sizeof(device_sockaddr), NULL);
 
-		// find interface whose netmask matches the camera's address
-		for (ifap_iter = ifap; ifap_iter != NULL; ifap_iter = ifap_iter->ifa_next) {
-			if ((ifap_iter->ifa_flags & IFF_UP) != 0 &&
-			(ifap_iter->ifa_flags & IFF_POINTOPOINT) == 0 &&
-			(ifap_iter->ifa_netmask != NULL) &&
-			(ifap_iter->ifa_addr != NULL) &&
-			(ifap_iter->ifa_addr->sa_family == AF_INET)) {
-				struct sockaddr_in *sa = (struct sockaddr_in *)ifap_iter->ifa_addr;
-				struct sockaddr_in *mask = (struct sockaddr_in *)ifap_iter->ifa_netmask;
-				if ((sa->sin_addr.s_addr & mask->sin_addr.s_addr) == (device_sockaddr.sin_addr.s_addr & mask->sin_addr.s_addr)) {
-					GSocketAddress *socket_address = g_socket_address_new_from_native(ifap_iter->ifa_addr, sizeof(struct sockaddr));
-					GInetAddress *inet_address = g_object_ref(g_inet_socket_address_get_address(G_INET_SOCKET_ADDRESS(socket_address)));
+		for (iface_iter = ifaces; iface_iter != NULL; iface_iter = iface_iter->next) {
+			struct sockaddr_in *sa = (struct sockaddr_in*)arv_network_interface_get_addr(iface_iter->data);
+			struct sockaddr_in *mask = (struct sockaddr_in*)arv_network_interface_get_netmask(iface_iter->data);
+			if ((sa->sin_addr.s_addr & mask->sin_addr.s_addr) == (device_sockaddr.sin_addr.s_addr & mask->sin_addr.s_addr)) {
+				GSocketAddress *socket_address = g_socket_address_new_from_native
+					(arv_network_interface_get_addr(iface_iter->data), sizeof(struct sockaddr));
+				GInetAddress *inet_address = g_object_ref(g_inet_socket_address_get_address
+									  (G_INET_SOCKET_ADDRESS(socket_address)));
 
-					freeifaddrs(ifap);
+				g_list_free_full (ifaces, (GDestroyNotify) arv_network_interface_free);
 
-					g_object_unref(socket_address);
-					g_object_unref(device_socket_address);
+				g_object_unref(socket_address);
+				g_object_unref(device_socket_address);
 
-					return inet_address;
-				}
+				return inet_address;
 			}
 		}
-
-		freeifaddrs(ifap);
+		g_list_free_full (ifaces, (GDestroyNotify) arv_network_interface_free);
 	}
 
 	socket_list = arv_gv_discover_socket_list_new();

src/arvgvstream.c
@@ -34,10 +34,11 @@
 #include <arvgvcpprivate.h>
 #include <arvdebug.h>
 #include <arvmisc.h>
+#include <arvmiscprivate.h>
+#include <arvnetworkprivate.h>
 #include <arvstr.h>
 #include <arvenumtypes.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <errno.h>
 
@@ -227,10 +228,10 @@ _update_socket (ArvGvStreamThreadData *thread_data, ArvBuffer *buffer)
 	}
 
 	if (buffer_size != thread_data->current_socket_buffer_size) {
-		int result;
+		gboolean result;
 
-		result = setsockopt (fd, SOL_SOCKET, SO_RCVBUF, &buffer_size, sizeof (buffer_size));
-		if (result == 0) {
+		result = arv_socket_set_recv_buffer_size (fd, buffer_size);
+		if (result) {
 			thread_data->current_socket_buffer_size = buffer_size;
 			arv_debug_stream_thread ("[GvStream::update_socket] Socket buffer size set to %d", buffer_size);
 		} else {

src/arvnetwork.c
@@ -1,0 +1,162 @@
+/* Aravis - Digital camera library
+ *
+ * Copyright © 2009-2019 Emmanuel Pacaud
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Author: Emmanuel Pacaud <emmanuel@gnome.org>
+ */
+
+#include <arvnetworkprivate.h>
+
+#ifndef G_OS_WIN32
+	#include <ifaddrs.h>
+#endif
+
+struct _ArvNetworkInterface{
+	struct sockaddr *addr;
+	struct sockaddr *netmask;
+	struct sockaddr *broadaddr;
+	char* name;
+};
+
+#ifdef G_OS_WIN32
+
+GList *
+arv_enumerate_network_interfaces (void)
+{
+	#warning arv_enumerate_network_interface not yet implemented for WIN32
+	return NULL;
+}
+
+/*
+ * mingw only defines inet_ntoa (ipv4-only), inet_ntop (IPv4 & IPv6) is missing from it headers
+ * therefore we define it ourselves; code comes from https://www.mail-archive.com/users@ipv6.org/msg02107.html
+ */
+
+const char *
+inet_ntop (int af, const void *src, char *dst, socklen_t cnt)
+{
+	if (af == AF_INET) {
+		struct sockaddr_in in;
+
+		memset (&in, 0, sizeof(in));
+		in.sin_family = AF_INET;
+		memcpy (&in.sin_addr, src, sizeof(struct in_addr));
+		getnameinfo ((struct sockaddr *)&in, sizeof (struct sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST);
+		return dst;
+	} else if (af == AF_INET6) {
+		struct sockaddr_in6 in;
+
+		memset (&in, 0, sizeof(in));
+		in.sin6_family = AF_INET6;
+		memcpy (&in.sin6_addr, src, sizeof(struct in_addr6));
+		getnameinfo ((struct sockaddr *)&in, sizeof (struct sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST);
+		return dst;
+	}
+
+	return NULL;
+}
+
+#else
+
+GList*
+arv_enumerate_network_interfaces (void)
+{
+	struct ifaddrs *ifap = NULL;
+	struct ifaddrs *ifap_iter;
+	GList* ret=NULL;
+
+	if (getifaddrs (&ifap) <0)
+		return NULL;
+
+	for (ifap_iter = ifap; ifap_iter != NULL; ifap_iter = ifap_iter->ifa_next) {
+		if ((ifap_iter->ifa_flags & IFF_UP) != 0 &&
+			(ifap_iter->ifa_flags & IFF_POINTOPOINT) == 0 &&
+			(ifap_iter->ifa_addr != NULL) &&
+			(ifap_iter->ifa_addr->sa_family == AF_INET)) {
+			ArvNetworkInterface* a;
+
+			a = g_new0 (ArvNetworkInterface, 1);
+
+			a->addr = g_memdup (ifap_iter->ifa_addr, sizeof(struct sockaddr));
+			if (ifap_iter->ifa_netmask)
+				a->netmask = g_memdup (ifap_iter->ifa_netmask, sizeof(struct sockaddr));
+			if (ifap_iter->ifa_ifu.ifu_broadaddr)
+				a->broadaddr = g_memdup(ifap_iter->ifa_ifu.ifu_broadaddr, sizeof(struct sockaddr));
+			if (ifap_iter->ifa_name)
+				a->name = g_strdup(ifap_iter->ifa_name);
+
+			ret = g_list_prepend (ret, a);
+		}
+	}
+
+	freeifaddrs (ifap);
+
+	return g_list_reverse (ret);
+};
+#endif
+
+struct sockaddr *
+arv_network_interface_get_addr(ArvNetworkInterface* a)
+{
+	return a->addr;
+}
+
+struct sockaddr *
+arv_network_interface_get_netmask(ArvNetworkInterface* a)
+{
+	return a->netmask;
+}
+
+struct sockaddr *
+arv_network_interface_get_broadaddr(ArvNetworkInterface* a)
+{
+	return a->broadaddr;
+}
+
+const char *
+arv_network_interface_get_name(ArvNetworkInterface* a)
+{
+	return a->name;
+}
+
+void
+arv_network_interface_free(ArvNetworkInterface *a) {
+	g_clear_pointer (&a->addr, g_free);
+	g_clear_pointer (&a->netmask, g_free);
+	g_clear_pointer (&a->broadaddr, g_free);
+	g_clear_pointer (&a->name, g_free);
+}
+
+
+gboolean
+arv_socket_set_recv_buffer_size (int socket_fd, gint buffer_size)
+{
+	int result;
+
+#ifndef G_OS_WIN32
+	result = setsockopt (socket_fd, SOL_SOCKET, SO_RCVBUF, &buffer_size, sizeof (buffer_size));
+#else
+	{
+		DWORD _buffer_size=buffer_size;
+		result = setsockopt (socket_fd, SOL_SOCKET, SO_RCVBUF, (const char*) &_buffer_size, sizeof (_buffer_size));
+	}
+#endif
+
+	return result == 0;
+}
+

src/arvnetworkprivate.h
@@ -1,0 +1,100 @@
+/* Aravis - Digital camera library
+ *
+ * Copyright © 2009-2019 Emmanuel Pacaud
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Author: Emmanuel Pacaud <emmanuel@gnome.org>
+ */
+
+#ifndef ARV_NETWORK_PRIVATE_H
+#define ARV_NETWORK_PRIVATE_H
+
+#ifndef __MINGW32__
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#endif
+
+#include <gio/gnetworking.h>
+
+#if !defined(__APPLE__) && !defined(G_OS_WIN32)
+#include <linux/ip.h>
+#endif
+
+#ifndef G_OS_WIN32
+#include <netinet/udp.h>
+#endif
+
+#ifdef G_OS_WIN32
+#include<stdint.h>
+typedef uint8_t u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+struct udphdr {
+	uint16_t source;
+	uint16_t dest;
+	uint16_t len;
+	uint16_t check;
+};
+#endif
+
+#if defined(__APPLE__) || defined(G_OS_WIN32)
+struct iphdr
+  {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ihl:4;
+    unsigned int version:4;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int version:4;
+    unsigned int ihl:4;
+#else
+# error  "Please fix <bits/endian.h>"
+#endif
+    u_int8_t tos;
+    u_int16_t tot_len;
+    u_int16_t id;
+    u_int16_t frag_off;
+    u_int8_t ttl;
+    u_int8_t protocol;
+    u_int16_t check;
+    u_int32_t saddr;
+    u_int32_t daddr;
+    /*The options start here. */
+  };
+#endif
+
+typedef struct _ArvNetworkInterface ArvNetworkInterface;
+
+GList *			arv_enumerate_network_interfaces	(void);
+
+void 			arv_network_interface_free		(ArvNetworkInterface *a);
+struct sockaddr *	arv_network_interface_get_addr		(ArvNetworkInterface *a);
+struct sockaddr *	arv_network_interface_get_netmask	(ArvNetworkInterface *a);
+struct sockaddr *	arv_network_interface_get_broadaddr	(ArvNetworkInterface *a);
+const char *		arv_network_interface_get_name		(ArvNetworkInterface *a);
+
+gboolean 		arv_socket_set_recv_buffer_size		(int socket_fd, gint buffer_size);
+
+#ifdef G_OS_WIN32
+/* prototype is missing on mingw, let's reimplement */
+const char *		inet_ntop				(int af, const void *src, char *dst, socklen_t cnt);
+#endif
+
+#endif

src/arvrealtime.c
@@ -1,4 +1,4 @@
-/***
+/*
   Copyright 2009 Lennart Poettering
   Copyright 2010 David Henningsson <diwic@ubuntu.com>
   Copyright 2014 Emmanuel Pacaud <emmanuel@gnome.org>
@@ -25,7 +25,7 @@
 
   This file is based on rtkit sources, ported from libdbus to glib gdbus API.
 
-***/
+*/
 
 #include <arvrealtimeprivate.h>
 #include <arvdebugprivate.h>
@@ -34,8 +34,10 @@
 #include <sched.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#ifndef G_OS_WIN32
 #include <sys/resource.h>
 #include <sys/syscall.h>
+#endif
 
 #define RTKIT_SERVICE_NAME "org.freedesktop.RealtimeKit1"
 #define RTKIT_OBJECT_PATH "/org/freedesktop/RealtimeKit1"
@@ -218,9 +220,14 @@ arv_rtkit_make_high_priority (GDBusConnection *connection, pid_t thread, int nic
 #define RLIMIT_RTTIME 15
 #endif
 
+#ifndef G_OS_WIN32
 static pid_t _gettid(void) {
         return (pid_t) syscall(SYS_gettid);
 }
+#else
+#include <processthreadsapi.h>
+static pid_t _gettid(void) { return GetCurrentThreadId(); }
+#endif
 
 /**
  * arv_make_thread_realtime:
@@ -234,7 +241,7 @@ static pid_t _gettid(void) {
  * Since: 0.4.0
  */
 
-#ifndef __APPLE__
+#if !defined(__APPLE__) && !defined(G_OS_WIN32)
 gboolean
 arv_make_thread_realtime (int priority)
 {
@@ -282,7 +289,7 @@ arv_make_thread_realtime (int priority)
 gboolean
 arv_make_thread_realtime (int priority)
 {
-	arv_debug_misc ("SCHED API not supported on OSX");
+	arv_debug_misc ("SCHED API not supported on OSX/Windows");
 
 	return FALSE;
 }

commit 15af691ba318118beaaa8e9c89ab35cb03c7d1bb

fakecamera: plug memory leak
d0de78275acf6d1c549e7aae91ef827dafcd10be introduced this issue, by
removing a check in the loop.

src/arvgvfakecamera.c
@@ -487,7 +487,7 @@ arv_gv_fake_camera_start (ArvGvFakeCamera *gv_fake_camera)
 		return FALSE;
 	}
 
-	for (iface_iter = ifaces; iface_iter != NULL; iface_iter = iface_iter->next) {
+	for (iface_iter = ifaces; iface_iter != NULL && !interface_found; iface_iter = iface_iter->next) {
 		GSocketAddress *socket_address;
 		GInetAddress *inet_address;
 		GInetAddress *gvcp_inet_address;