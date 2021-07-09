76254b7faf59901166ec78a73e94b03a0cd8bb77
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2021-07-09 19:21:12 +0200

    acquisitiontest: check for buffer status

--- tests/arvacquisitiontest.c
+++ tests/arvacquisitiontest.c
@@ -11,7 +11,7 @@ main (int argc, char **argv)
 	camera = arv_camera_new (argc > 1 ? argv[1] : NULL, NULL);
 	buffer = arv_camera_acquisition (camera, 0, NULL);
 
-	if (ARV_IS_BUFFER (buffer))
+	if (ARV_IS_BUFFER (buffer) && arv_buffer_get_status (buffer) == ARV_BUFFER_STATUS_SUCCESS)
 		printf ("Image successfully acquired\n");
 	else
 		printf ("Failed to acquire a single image\n");
