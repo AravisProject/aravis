80a382376297d7ca048ce09a7af7270fac6dc1d6
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2021-09-10 14:25:22 +0200

    tool: filter features using extended glob pattern

--- src/arvtool.c
+++ src/arvtool.c
@@ -21,6 +21,7 @@
  */
 
 #include <arvdebugprivate.h>
+#include <arvmiscprivate.h>
 #include <arv.h>
 #include <stdlib.h>
 #include <string.h>
@@ -96,96 +97,117 @@ typedef enum {
 } ArvToolListMode;
 
 static void
-arv_tool_list_features (ArvGc *genicam, const char *feature, ArvToolListMode list_mode, int level)
+arv_tool_list_features (ArvGc *genicam, const char *feature, ArvToolListMode list_mode, GRegex *regex, int level)
 {
 	ArvGcNode *node;
 
 	node = arv_gc_get_node (genicam, feature);
 	if (ARV_IS_GC_FEATURE_NODE (node) &&
 	    arv_gc_feature_node_is_implemented (ARV_GC_FEATURE_NODE (node), NULL)) {
+                gboolean match;
 
-		if (ARV_IS_GC_CATEGORY (node)) {
-			printf ("%*s%-12s: '%s'\n", 4 * level, "",
-				arv_dom_node_get_node_name (ARV_DOM_NODE (node)),
-				feature);
-		} else {
-			if (arv_gc_feature_node_is_available (ARV_GC_FEATURE_NODE (node), NULL)) {
-				char *value = NULL;
-				GError *error = NULL;
-				gboolean is_selector;
+                match = regex == NULL || g_regex_match (regex,
+                                                        arv_gc_feature_node_get_name (ARV_GC_FEATURE_NODE (node)),
+                                                        0, NULL);
+                if (match) {
+                        if (ARV_IS_GC_CATEGORY (node)) {
+                                printf ("%*s%-12s: '%s'\n", 4 * level, "",
+                                        arv_dom_node_get_node_name (ARV_DOM_NODE (node)),
+                                        feature);
+                        } else {
+                                if (arv_gc_feature_node_is_available (ARV_GC_FEATURE_NODE (node), NULL)) {
+                                        char *value = NULL;
+                                        GError *error = NULL;
+                                        gboolean is_selector;
 
-				if (list_mode == ARV_TOOL_LIST_MODE_VALUES) {
-					const char *unit;
+                                        if (list_mode == ARV_TOOL_LIST_MODE_VALUES) {
+                                                const char *unit;
 
-					if (ARV_IS_GC_STRING (node) ||
-					    ARV_IS_GC_ENUMERATION (node)) {
-						value = g_strdup_printf ("'%s'", arv_gc_string_get_value (ARV_GC_STRING (node), &error));
-					} else if (ARV_IS_GC_INTEGER (node)) {
-						if (ARV_IS_GC_ENUMERATION (node)) {
-							value = g_strdup_printf ("'%s'",
-										 arv_gc_string_get_value (ARV_GC_STRING (node), &error));
-						} else {
-							unit = arv_gc_integer_get_unit (ARV_GC_INTEGER (node));
+                                                if (ARV_IS_GC_STRING (node) ||
+                                                    ARV_IS_GC_ENUMERATION (node)) {
+                                                        value = g_strdup_printf ("'%s'", arv_gc_string_get_value (ARV_GC_STRING (node), &error));
+                                                } else if (ARV_IS_GC_INTEGER (node)) {
+                                                        if (ARV_IS_GC_ENUMERATION (node)) {
+                                                                value = g_strdup_printf ("'%s'",
+                                                                                         arv_gc_string_get_value (ARV_GC_STRING (node), &error));
+                                                        } else {
+                                                                unit = arv_gc_integer_get_unit (ARV_GC_INTEGER (node));
 
-							value = g_strdup_printf ("%" G_GINT64_FORMAT "%s%s",
-										 arv_gc_integer_get_value (ARV_GC_INTEGER (node), &error),
-										 unit != NULL ? " " : "",
-										 unit != NULL ? unit : "");
-						}
-					} else if (ARV_IS_GC_FLOAT (node)) {
-						unit = arv_gc_float_get_unit (ARV_GC_FLOAT (node));
+                                                                value = g_strdup_printf ("%" G_GINT64_FORMAT "%s%s",
+                                                                                         arv_gc_integer_get_value (ARV_GC_INTEGER (node), &error),
+                                                                                         unit != NULL ? " " : "",
+                                                                                         unit != NULL ? unit : "");
+                                                        }
+                                                } else if (ARV_IS_GC_FLOAT (node)) {
+                                                        unit = arv_gc_float_get_unit (ARV_GC_FLOAT (node));
 
-						value = g_strdup_printf ("%g%s%s",
-									 arv_gc_float_get_value (ARV_GC_FLOAT (node), &error),
-									 unit != NULL ? " " : "",
-									 unit != NULL ? unit : "");
-					} else if (ARV_IS_GC_BOOLEAN (node)) {
-						value = g_strdup_printf ("%s",
-									 arv_gc_boolean_get_value (ARV_GC_BOOLEAN (node), &error) ?
-									 "true" : "false");
-					}
-				}
+                                                        value = g_strdup_printf ("%g%s%s",
+                                                                                 arv_gc_float_get_value (ARV_GC_FLOAT (node), &error),
+                                                                                 unit != NULL ? " " : "",
+                                                                                 unit != NULL ? unit : "");
+                                                } else if (ARV_IS_GC_BOOLEAN (node)) {
+                                                        value = g_strdup_printf ("%s",
+                                                                                 arv_gc_boolean_get_value (ARV_GC_BOOLEAN (node), &error) ?
+                                                                                 "true" : "false");
+                                                }
+                                        }
 
-				is_selector = ARV_IS_GC_SELECTOR (node) && arv_gc_selector_is_selector (ARV_GC_SELECTOR (node));
+                                        is_selector = ARV_IS_GC_SELECTOR (node) && arv_gc_selector_is_selector (ARV_GC_SELECTOR (node));
 
-				if (error != NULL) {
-					g_clear_error (&error);
-				} else {
-					if (value != NULL && value[0] != '\0')
-						printf ("%*s%-12s: '%s' = %s\n", 4 * level, "",
-							arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature, value);
-					else
-						printf ("%*s%-12s: '%s'\n", 4 * level, "",
-							arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature);
+                                        if (error != NULL) {
+                                                g_clear_error (&error);
+                                        } else {
+                                                if (value != NULL && value[0] != '\0')
+                                                        printf ("%*s%-12s: '%s' = %s\n", 4 * level, "",
+                                                                arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature, value);
+                                                else
+                                                        printf ("%*s%-12s: '%s'\n", 4 * level, "",
+                                                                arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature);
 
-					if (is_selector) {
-						const GSList *iter;
+                                                if (is_selector) {
+                                                        const GSList *iter;
 
-						for (iter = arv_gc_selector_get_selected_features (ARV_GC_SELECTOR (node));
-						     iter != NULL;
-						     iter = iter->next) {
-							printf (" %*s     * %s\n", 4 * level, " ",
-								arv_gc_feature_node_get_name (iter->data));
-						}
+                                                        for (iter = arv_gc_selector_get_selected_features (ARV_GC_SELECTOR (node));
+                                                             iter != NULL;
+                                                             iter = iter->next) {
+                                                                printf (" %*s     * %s\n", 4 * level, " ",
+                                                                        arv_gc_feature_node_get_name (iter->data));
+                                                        }
 
-					}
-				}
+                                                }
+                                        }
 
-				g_clear_pointer (&value, g_free);
-			} else {
-				if (list_mode == ARV_TOOL_LIST_MODE_FEATURES)
-					printf ("%*s%-12s: '%s' (Not available)\n", 4 * level, "",
-						arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature);
-			}
-		}
+                                        g_clear_pointer (&value, g_free);
+                                } else {
+                                        if (list_mode == ARV_TOOL_LIST_MODE_FEATURES)
+                                                printf ("%*s%-12s: '%s' (Not available)\n", 4 * level, "",
+                                                        arv_dom_node_get_node_name (ARV_DOM_NODE (node)), feature);
+                                }
+                        }
 
-		if (list_mode == ARV_TOOL_LIST_MODE_DESCRIPTIONS) {
-			const char *description;
+                        if (list_mode == ARV_TOOL_LIST_MODE_DESCRIPTIONS) {
+                                const char *description;
 
-			description = arv_gc_feature_node_get_description (ARV_GC_FEATURE_NODE (node));
-			if (description)
-				printf ("%s\n", description);
-		}
+                                description = arv_gc_feature_node_get_description (ARV_GC_FEATURE_NODE (node));
+                                if (description)
+                                        printf ("%s\n", description);
+                        }
+
+                        if (ARV_IS_GC_ENUMERATION (node) && list_mode == ARV_TOOL_LIST_MODE_FEATURES) {
+                                const GSList *childs;
+                                const GSList *iter;
+
+                                childs = arv_gc_enumeration_get_entries (ARV_GC_ENUMERATION (node));
+                                for (iter = childs; iter != NULL; iter = iter->next) {
+                                        if (arv_gc_feature_node_is_implemented (iter->data, NULL)) {
+                                                printf ("%*s%-12s: '%s'%s\n", 4 * (level + 1), "",
+                                                        arv_dom_node_get_node_name (iter->data),
+                                                        arv_gc_feature_node_get_name (iter->data),
+                                                        arv_gc_feature_node_is_available (iter->data, NULL) ? "" : " (Not available)");
+                                        }
+                                }
+                        }
+                }
 
 		if (ARV_IS_GC_CATEGORY (node)) {
 			const GSList *features;
@@ -194,21 +216,8 @@ arv_tool_list_features (ArvGc *genicam, const char *feature, ArvToolListMode lis
 			features = arv_gc_category_get_features (ARV_GC_CATEGORY (node));
 
 			for (iter = features; iter != NULL; iter = iter->next)
-				arv_tool_list_features (genicam, iter->data, list_mode, level + 1);
-		} else if (ARV_IS_GC_ENUMERATION (node) && list_mode == ARV_TOOL_LIST_MODE_FEATURES) {
-			const GSList *childs;
-			const GSList *iter;
-
-			childs = arv_gc_enumeration_get_entries (ARV_GC_ENUMERATION (node));
-			for (iter = childs; iter != NULL; iter = iter->next) {
-				if (arv_gc_feature_node_is_implemented (iter->data, NULL)) {
-					printf ("%*s%-12s: '%s'%s\n", 4 * (level + 1), "",
-						arv_dom_node_get_node_name (iter->data),
-						arv_gc_feature_node_get_name (iter->data),
-						arv_gc_feature_node_is_available (iter->data, NULL) ? "" : " (Not available)");
-				}
-			}
-		}
+				arv_tool_list_features (genicam, iter->data, list_mode, match ? NULL : regex, level + 1);
+                }
 	}
 }
 
@@ -239,30 +248,35 @@ arv_tool_execute_command (int argc, char **argv, ArvDevice *device,
 		if (xml != NULL)
 			printf ("%*s\n", (int) size, xml);
 	} else if (g_strcmp0 (command, "features") == 0) {
-		arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_FEATURES, 0);
+                if (argc > 3)
+                        printf ("features command takes at most one feature selection parameter\n");
+                else {
+                        GRegex *regex;
+
+                        regex = arv_regex_new_from_glob_pattern (argc == 3 ? argv[2] : "*", TRUE);
+                        arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_FEATURES,  regex, 0);
+                        g_regex_unref (regex);
+                }
 	} else if (g_strcmp0 (command, "values") == 0) {
-		arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_VALUES, 0);
-	} else if (g_strcmp0 (command, "description") == 0) {
-		if (argc < 3)
-			arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_DESCRIPTIONS, 0);
-		else {
-			int i;
+                if (argc > 3)
+                        printf ("features command takes at most one feature selection parameter\n");
+                else {
+                        GRegex *regex;
 
-			for (i = 2; i < argc; i++) {
-				ArvGcNode *node;
+                        regex = arv_regex_new_from_glob_pattern (argc == 3 ? argv[2] : "*", TRUE);
+                        arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_VALUES, regex, 0);
+                        g_regex_unref (regex);
+                }
+        } else if (g_strcmp0 (command, "description") == 0) {
+                if (argc > 3)
+                        printf ("features command takes at most one feature selection parameter\n");
+                else {
+                        GRegex *regex;
 
-				node = arv_gc_get_node (genicam, argv[i]);
-				if (ARV_IS_GC_NODE (node)) {
-					const char *description;
-
-					printf ("%s: '%s'\n", arv_dom_node_get_node_name (ARV_DOM_NODE (node)), argv[i]);
-
-					description = arv_gc_feature_node_get_description (ARV_GC_FEATURE_NODE (node));
-					if (description)
-						printf ("%s\n", description);
-				}
-			}
-		}
+                        regex = arv_regex_new_from_glob_pattern (argc == 3 ? argv[2] : "*", TRUE);
+                        arv_tool_list_features (genicam, "Root", ARV_TOOL_LIST_MODE_DESCRIPTIONS, regex, 0);
+                        g_regex_unref (regex);
+                }
 	} else if (g_strcmp0 (command, "control") == 0) {
 		int i;
 
