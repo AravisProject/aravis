d73ad58434e11780e8458aa1400d0522c0d88b7a
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2022-07-13 17:05:57 +0200

    linux: gigevision kernel driver build setup
    It is just an empty driver for now :)

---
+++ drivers/mesonbuildkmod.sh
@@ -0,0 +1,7 @@
+mkdir -p "$2/drivers/kmod.dir"
+cp -Rf   "$1/drivers/"* "$2/drivers/kmod.dir"
+rm -f    "$2/drivers/kmod.dir/meson.build" "$2/drivers/kmod.dir/mesonbuild.sh"
+cd       "$2/drivers/kmod.dir"
+sed -i -e "s|\$(src)|$1/drivers|" "$2/drivers/kmod.dir/Kbuild"
+make
+mv       "aravis_gv.ko" "$2/drivers"
