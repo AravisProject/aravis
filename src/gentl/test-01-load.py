e96968297a31bdddce41c6bf72bcc22a5e651f67
Václav Šmilauer <eu@doxos.eu>
2023-10-14 21:46:14 +0200

    Add <stdint.h> (needed for MinGW, says CI)

--- src/gentl/test-01-load.py
+++ src/gentl/test-01-load.py
@@ -2,4 +2,5 @@ from harvesters.core import Harvester
 H=Harvester()
 H.add_file('../../build/src/gentl/aravis-0.8.30.cti')
 H.update()
-print(H.device_info_list)
+from rich.pretty import pprint
+pprint(H.device_info_list)
