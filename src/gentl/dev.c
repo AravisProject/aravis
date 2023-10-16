104c9d5c033506e292061398bcbdbf10114d5857
Václav Šmilauer <eu@doxos.eu>
2023-10-16 07:49:47 +0200

    Implement further ArvCamera/ArvDevice related functions; stuck at event support now (nothing comparable?)

--- src/gentl/dev.c
+++ src/gentl/dev.c
@@ -1,9 +1,52 @@
 #include"private.h"
-GC_API DevGetPort              ( DEV_HANDLE hDevice, PORT_HANDLE *phRemoteDevice ){ GENTL_NYI; }
-GC_API DevGetNumDataStreams    ( DEV_HANDLE hDevice, uint32_t *piNumDataStreams ){ GENTL_NYI; }
+#include<arvcamera.h>
+
+/*
+GenTL's Device is Aravis' Camera. GenTL's Device port is Aravis' Device.
+*/
+
+#define _DEV_CHECK_HANDLE GENTL_ENSURE_INIT; if(hDevice==NULL || !(ARV_IS_CAMERA(hDevice))) return GC_ERR_INVALID_HANDLE;
+
+/*
+helper function to get string from camera via function all (e.g. arv_camera_get_device_id) and return it to the GenTL buffer
+*/
+GC_ERROR gentl_to_buf__cam_string(DEV_HANDLE hDevice, const char* (*func)(ArvCamera*,GError**), INFO_DATATYPE *piType, void *pBuffer, size_t *piSize);
+
+GC_ERROR gentl_to_buf__cam_string(DEV_HANDLE hDevice, const char* (*func)(ArvCamera*,GError**), INFO_DATATYPE *piType, void *pBuffer, size_t *piSize){
+	const char* n;
+	n=func(hDevice,&gentl_err);
+	if(n==NULL) return GC_ERR_IO;
+	return gentl_to_buf(INFO_DATATYPE_STRING,pBuffer,n,piSize,piType);
+};
+
+GC_API DevGetPort              ( DEV_HANDLE hDevice, PORT_HANDLE *phRemoteDevice ){
+	_DEV_CHECK_HANDLE;
+	arv_trace_gentl("%s (hDevice=%s[%p])",__FUNCTION__,G_OBJECT_TYPE_NAME(hDevice),hDevice);
+	*phRemoteDevice=arv_camera_get_device(hDevice);
+	return GC_ERR_SUCCESS;
+}
+
+GC_API DevGetNumDataStreams    ( DEV_HANDLE hDevice, uint32_t *piNumDataStreams ){
+	_DEV_CHECK_HANDLE
+	arv_trace_gentl("%s (hDevice=%s[%p])",__FUNCTION__,G_OBJECT_TYPE_NAME(hDevice),hDevice);
+	*piNumDataStreams=arv_camera_gv_get_n_stream_channels(hDevice,&gentl_err);
+	if(*piNumDataStreams==0) return GC_ERR_IO;
+	return GC_ERR_SUCCESS;
+}
 GC_API DevGetDataStreamID      ( DEV_HANDLE hDevice, uint32_t iIndex, char *sDataStreamID, size_t *piSize ){ GENTL_NYI; }
 GC_API DevOpenDataStream       ( DEV_HANDLE hDevice, const char *sDataStreamID, DS_HANDLE *phDataStream ){ GENTL_NYI; }
-GC_API DevGetInfo              ( DEV_HANDLE hDevice, DEVICE_INFO_CMD iInfoCmd, INFO_DATATYPE *piType, void *pBuffer, size_t *piSize ){ GENTL_NYI; }
+
+GC_API DevGetInfo              ( DEV_HANDLE hDevice, DEVICE_INFO_CMD iInfoCmd, INFO_DATATYPE *piType, void *pBuffer, size_t *piSize ){
+	_DEV_CHECK_HANDLE;
+	arv_trace_gentl("%s (hDevice=%s[%p],iInfoCmd=%d)",__FUNCTION__,G_OBJECT_TYPE_NAME(hDevice),hDevice,iInfoCmd);
+	switch(iInfoCmd){
+		case DEVICE_INFO_ID:
+			return gentl_to_buf__cam_string(hDevice,arv_camera_get_device_id,piType,pBuffer,piSize);
+		default:
+			GENTL_NYI_DETAIL("iInfoCmd=%d",iInfoCmd);
+	}
+}
+
 GC_API DevClose                ( DEV_HANDLE hDevice ){ GENTL_NYI; }
 /* GenTL v1.4 */
 GC_API DevGetParentIF          ( DEV_HANDLE hDevice, IF_HANDLE *phIface ){ GENTL_NYI; }
--- src/gentl/private.c
+++ src/gentl/private.c
@@ -18,9 +18,14 @@ gentl_error_quark (void)
 
 size_t gentl_buf_size(INFO_DATATYPE type, const void* data){
 	switch(type){
-		case INFO_DATATYPE_UNKNOWN: return 1; /* ?? */
-		case INFO_DATATYPE_STRING: return strlen(data)+1;
-		case INFO_DATATYPE_STRINGLIST: g_critical("%s: returning zero for for INFO_DATATYPE_STRINGLIST (not implemented), expect trouble!",__FUNCTION__); return 0;
+		case INFO_DATATYPE_UNKNOWN: return 0;
+		case INFO_DATATYPE_STRING:  return strlen(data)+1;
+		case INFO_DATATYPE_STRINGLIST:{
+			/* TODO: check for correctness */
+			char *s=(char*)data;
+			while(s[0]!='\0') s+=strlen(s)+1;
+			return s-(char*)data;
+		}
 		case INFO_DATATYPE_INT16:
 		case INFO_DATATYPE_UINT16:
 			return 2;
@@ -31,14 +36,12 @@ size_t gentl_buf_size(INFO_DATATYPE type, const void* data){
 		case INFO_DATATYPE_UINT64:
 		case INFO_DATATYPE_FLOAT64:
 			return 8;
-		case INFO_DATATYPE_PTR: return sizeof(void*);
-		case INFO_DATATYPE_BOOL8: return 1;
-		case INFO_DATATYPE_SIZET: return sizeof(size_t);
-		case INFO_DATATYPE_BUFFER:
-			g_critical("%s: returning zero for for INFO_DATATYPE_BUFFER (not implemented), expect trouble!",__FUNCTION__);
-			return 0;
-		case INFO_DATATYPE_PTRDIFF: return sizeof(ptrdiff_t);
-		default: return 0;
+		case INFO_DATATYPE_PTR:    return sizeof(void*);
+		case INFO_DATATYPE_BOOL8:  return 1;
+		case INFO_DATATYPE_SIZET:  return sizeof(size_t);
+		case INFO_DATATYPE_BUFFER: return 0;
+		case INFO_DATATYPE_PTRDIFF:return sizeof(ptrdiff_t);
+		default:                   return 0;
 	}
 }
 
@@ -48,7 +51,7 @@ GC_ERROR gentl_to_buf(INFO_DATATYPE type, void* dst, const void* src, size_t* sz
 	if(sz==NULL) return GC_ERR_INVALID_PARAMETER;
 	if(piType!=NULL) *piType=type;
 	szSrc=gentl_buf_size(type,src);
-	if(sz==0){ arv_warning_gentl("Datatype %d: zero size (buffer copy unhandled).",type); return GC_ERR_NOT_IMPLEMENTED; }
+	if(sz==0){ arv_warning_gentl("Datatype %d: zero size?",type); }
 	/* the call only queries about necessary storage */
 	if(dst==NULL){
 		*sz=szSrc;
@@ -58,30 +61,62 @@ GC_ERROR gentl_to_buf(INFO_DATATYPE type, void* dst, const void* src, size_t* sz
 	if(szSrc>*sz) return GC_ERR_BUFFER_TOO_SMALL;
 	*sz=szSrc;
 	switch(type){
+		case INFO_DATATYPE_UNKNOWN:
+			gentl_err = g_error_new (GENTL_ERROR, GC_ERR_INVALID_PARAMETER, "%s: INFO_DATATYPE_UNKNOWN is not allowed.", __FUNCTION__);
+			return GC_ERR_INVALID_PARAMETER;
 		case INFO_DATATYPE_STRING:
 			arv_trace_gentl("   (returning %ld-byte string: '%s')",szSrc,(const char*)src);
 			strcpy(dst,src);
 			break;
+		case INFO_DATATYPE_STRINGLIST:
+			GENTL_NYI_DETAIL("%s","INFO_DATATYPE_STRINGLIST");
 		case INFO_DATATYPE_INT16:
-			*(int16_t*)dst=*(int16_t*)src; break;
+			*(int16_t*)dst=*(int16_t*)src;
 			arv_trace_gentl("   (returning int16_t: %d",*(int16_t*)dst);
 			break;
-		case INFO_DATATYPE_UINT16: *(uint16_t*)dst=*(uint16_t*)src; break;
+		case INFO_DATATYPE_UINT16:
+			*(uint16_t*)dst=*(uint16_t*)src;
+			arv_trace_gentl("   (returning uint16_t: %d",*(uint16_t*)dst);
+			break;
 		case INFO_DATATYPE_INT32:
-			*(int32_t*)dst=*(int32_t*)src; break;
+			*(int32_t*)dst=*(int32_t*)src;
 			arv_trace_gentl("   (returning int32_t: %d",*(int32_t*)dst);
 			break;
 		case INFO_DATATYPE_UINT32:
 			*(uint32_t*)dst=*(uint32_t*)src;
 			arv_trace_gentl("   (returning uint32_t: %d",*(uint32_t*)dst);
 			break;
-		case INFO_DATATYPE_INT64:  *(int64_t*)dst=*(int64_t*)src; break;
-		case INFO_DATATYPE_UINT64: *(uint64_t*)dst=*(uint64_t*)src; break;
-		case INFO_DATATYPE_FLOAT64:*(double*)dst=*(double*)src; break;
-		case INFO_DATATYPE_PTR:    dst=(void*)src; break;
-		case INFO_DATATYPE_BOOL8:  *(char*)dst=*(char*)src; break;
-		case INFO_DATATYPE_SIZET:  *(size_t*)dst=*(size_t*)src; break;
-		case INFO_DATATYPE_PTRDIFF:*(ptrdiff_t*)dst=*(ptrdiff_t*)src; break;
+		case INFO_DATATYPE_INT64:
+			*(int64_t*)dst=*(int64_t*)src;
+			arv_trace_gentl("   (returning int64_t: %ld",*(int64_t*)dst);
+			break;
+		case INFO_DATATYPE_UINT64:
+			*(uint64_t*)dst=*(uint64_t*)src; break;
+			arv_trace_gentl("   (returning uint64_t: %ld",*(uint64_t*)dst);
+			break;
+		case INFO_DATATYPE_FLOAT64:
+			*(double*)dst=*(double*)src; break;
+			arv_trace_gentl("   (returning uint64_t: %lf",*(double*)dst);
+			break;
+		case INFO_DATATYPE_PTR:
+			dst=(void*)src;
+			arv_trace_gentl("   (returning pointer: %p",dst);
+			break;
+		case INFO_DATATYPE_BOOL8:
+			arv_trace_gentl("   (returning bool8: %u",*(char*)dst);
+			*(char*)dst=*(char*)src; break;
+			break;
+		case INFO_DATATYPE_SIZET:
+			*(size_t*)dst=*(size_t*)src; break;
+			arv_trace_gentl("   (returning size_t: %ld",*(size_t*)dst);
+			break;
+		case INFO_DATATYPE_BUFFER:
+			GENTL_NYI_DETAIL("%s","INFO_DATATYPE_BUFFER");
+			/* memcpy(dst,src,szSrc); break; */
+		case INFO_DATATYPE_PTRDIFF:
+			*(ptrdiff_t*)dst=*(ptrdiff_t*)src; break;
+			arv_trace_gentl("   (returning ptrdiff_t: %td",*(ptrdiff_t*)dst);
+			break;
 		default:
 			arv_warning_gentl("Datatype %d: buffer copy not implemented.",type); return GC_ERR_NOT_IMPLEMENTED;
 	}
