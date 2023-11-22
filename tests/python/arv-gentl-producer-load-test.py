42a77c8e05a07c98c4191837ef18ce67e01fb77f
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2023-11-22 09:37:57 +0100

    producer: cleanups

--- tests/python/arv-gentl-producer-load-test.py
+++ tests/python/arv-gentl-producer-load-test.py
@@ -1,6 +1,10 @@
 from harvesters.core import Harvester
+import os
+
 H=Harvester()
-H.add_file('../../build/src/gentl/aravis-0.8.30.cti')
+H.add_file(os.environ.get('ARV_PRODUCER_PATH'))
 H.update()
+
 from rich.pretty import pprint
+
 pprint(H.device_info_list)
