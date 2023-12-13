9572bda233573f9166b997c40a7f30430f5c2477
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2023-12-13 17:50:37 +0100

    v4l2: rowstride

--- src/arvv4l2deviceprivate.h
+++ src/arvv4l2deviceprivate.h
@@ -34,7 +34,7 @@ G_BEGIN_DECLS
 gboolean        arv_v4l2_device_set_image_format        (ArvV4l2Device *device);
 gboolean        arv_v4l2_device_get_image_format        (ArvV4l2Device *device,
                                                          guint32 *payload_size, guint32 *pixel_format,
-                                                         guint32 *width, guint32 *height);
+                                                         guint32 *width, guint32 *height, guint32 *bytes_per_line);
 int             arv_v4l2_device_get_fd                  (ArvV4l2Device *v4l2_device);
 
 G_END_DECLS
