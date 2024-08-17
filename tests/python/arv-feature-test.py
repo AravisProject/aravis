977245646c08e078f287b8cfdb26e459e0865dc3
Marko Kohtala <marko.kohtala@okoko.fi>
2024-08-17 23:32:23 +0300

    device: add generic feature read and write methods
    Dynamic language bindings can accept any type of a value so there is no
    need to select correct method.

--- tests/python/arv-feature-test.py
+++ tests/python/arv-feature-test.py
@@ -14,8 +14,9 @@ gi.require_version ('Aravis', '0.8')
 
 from gi.repository import Aravis
 
+Aravis.enable_interface("Fake")
 try:
-    camera = Aravis.Camera.new (None)
+    camera = Aravis.Camera(name="Fake_1")
 except TypeError:
     print ("No camera found")
     exit ()
@@ -27,3 +28,26 @@ device.set_integer_feature_value ("Height", 1024)
 
 print ("Width =  %d" %(device.get_integer_feature_value ("Width")))
 print ("Height = %d" %(device.get_integer_feature_value ("Height")))
+
+
+def pt(x):
+    print(type(x), x)
+
+pt(device.get_feature_value("DeviceVersion"))
+device.set_feature_value("Width", 640.0)
+pt(device.get_feature_value("Width"))
+device.set_feature_value("AcquisitionFrameRate", 2)
+pt(device.get_feature_value("AcquisitionFrameRate"))
+device.set_feature_value("TestBoolean", True)
+pt(device.get_feature_value("TestBoolean"))
+try:
+    device.set_feature_value("TestBoolean", None)
+except TypeError:
+    pass
+else:
+    print("Expected TypeError")
+device.set_feature_value("TestBoolean", 0)
+pt(device.get_feature_value("TestBoolean"))
+device.set_feature_value("TestStringReg", "TestValue")
+pt(device.get_feature_value("TestStringReg"))
+pt(device.get_feature_value("GainAuto"))
